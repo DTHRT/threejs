0f2bfcab84f788522dbf475ef7683db88cc1ea13
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2022-11-04 22:59:14 +0900

    feat: add planet with basic animation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import html from './index.html';
 import './style.css';
+
+import globe from './images/earth.jpg';
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+// Create a sphere
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(globe),
+  })
+);
+
+scene.add(sphere);
+
+camera.position.z = 15;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  sphere.rotation.y += 0.001;
+}
+animate();
